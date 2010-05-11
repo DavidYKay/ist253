c22284b2adb69e2f0a8abf6721e66e81e2e9e509
David Y. Kay <davidykay@gmail.com>
2010-05-11 19:39:21 -0400

    1.0 - about to roll up my sleeves and do it big

--- cust/newcustomer.php
+++ cust/newcustomer.php
@@ -2,8 +2,12 @@
 
 include "include.php";
 
+/*
+ * Create a new customer based on parameters taken in from the web form
+ */
 function newCustomer($fname, $lname, $stadd, $stadd2, $city, $state, $zip, $phone) {
-	//read in from test
+    //echo "Params received: ";
+    //echo $fname . $lname . $stadd . $stadd2 . $city . $state . $zip . $phone;
 	$mysqli = new mysqli( $db_host, $db_user, $db_password, 'test');
 	$mysqli->select_db('test');
 
@@ -12,34 +16,16 @@ function newCustomer($fname, $lname, $stadd, $stadd2, $city, $state, $zip, $phon
 		exit;
 	}
 
-	/*
-	#if ($result = $mysqli->query('SELECT Name, Population FROM City ORDER BY Population DESC LIMIT 5')) {
-	if ($result = $mysqli->query('SELECT * FROM CUSTOMER')) {
-
-		print("Customers are:\n");
-		echo "<br />";
-		//Array
-		$customers = array();
-		#while( $row = $result->fetch_assoc() ){
-		while( $row = $result->fetch_array() ){
-			//printf("%s (%s)\n", $row['Name'], $row['Population']);
-			array_push($customers, $row);
-		}
-		$result->close();
-		//Switch over to ours
-		$mysqli->select_db($db_database);
-     */
-
     if ($stmt = $mysqli->prepare("INSERT INTO `CUSTOMER` 
         (CustomerLName, CustomerFName, CustomerAddress, CustomerCity, CustomerState, CustomerZip, CustomerPhone)
         VALUES ('?', '?', '?', '?', '?', '?', '?')")
     ) {
         $address = $stadd . "\n" . $stadd2;
-        #$stmt->bind_param('sssssss', $fname, $lname, $address, $city, $state, $zip, $phone);
         $stmt->bind_param('sssssss', $fname, $lname, $address, $city, $state, $zip, $phone);
         //sssd', $code, $language, $official, $percent
         $stmt->execute(); 
         echo "Statement SUCCESS";
+        echo "<br/>";
     } else {
         echo "Statement FAIL: $stmt";
         echo "<br />";
@@ -64,5 +50,7 @@ $phone  = $_POST['element_4_1'] .  $_POST['element_4_2'] .  $_POST['element_4_3'
 if ($fname && $lname) {
     newCustomer($fname, $lname, $stadd, $stadd2, $city, $state, $zip, $phone);
 }
+
+readfile("../footer.html");
 
 ?>
