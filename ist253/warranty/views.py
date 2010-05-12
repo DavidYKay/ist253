f8a77afff42cb3708e92fbe3befce547aff36274
David Y. Kay <davidykay@gmail.com>
2010-05-12 04:18:31 -0400

    All basic listings up

--- ist253/warranty/views.py
+++ ist253/warranty/views.py
@@ -85,9 +85,9 @@ def diagnostic_list(request):
     return HttpResponse(t.render(c))
     #return HttpResponse("Yahoo")
 
-def diagnostic_detail(request, partno):
+def diagnostic_detail(request, diagno):
     diagnostic_list = Diagnostic.objects.all()
-    diagnostic = get_object_or_404(Diagnostic, pk=partno)
+    diagnostic = get_object_or_404(Diagnostic, pk=diagno)
     t = loader.get_template('warranty/diagnostic_detail.html')
     c = Context({
         'diagnostic': diagnostic,
@@ -96,8 +96,8 @@ def diagnostic_detail(request, partno):
 
 
 def invoice_list(request):
-    #invoice_list = Invoice.objects.all().order_by('partno')
-    invoice_list = Invoice.objects.all()
+    invoice_list = Invoice.objects.all().order_by('invno')
+    #invoice_list = Invoice.objects.all()
     t = loader.get_template('warranty/invoice_list.html')
     c = Context({
         'invoice_list': invoice_list,
@@ -105,9 +105,9 @@ def invoice_list(request):
     return HttpResponse(t.render(c))
     #return HttpResponse("Yahoo")
 
-def invoice_detail(request, partno):
+def invoice_detail(request, invno):
     invoice_list = Invoice.objects.all()
-    invoice = get_object_or_404(Invoice, pk=partno)
+    invoice = get_object_or_404(Invoice, pk=invno)
     t = loader.get_template('warranty/invoice_detail.html')
     c = Context({
         'invoice': invoice,
@@ -116,8 +116,8 @@ def invoice_detail(request, partno):
 
 
 def test_list(request):
-    #test_list = TestResults.objects.all().order_by('partno')
-    test_list = TestResults.objects.all()
+    test_list = TestResults.objects.all().order_by('testresno')
+    #test_list = TestResults.objects.all()
     t = loader.get_template('warranty/test_list.html')
     c = Context({
         'test_list': test_list,
@@ -125,11 +125,31 @@ def test_list(request):
     return HttpResponse(t.render(c))
     #return HttpResponse("Yahoo")
 
-def test_detail(request, partno):
+def test_detail(request, testresno):
     test_list = TestResults.objects.all()
-    test = get_object_or_404(TestResults, pk=partno)
+    test = get_object_or_404(TestResults, pk=testresno)
     t = loader.get_template('warranty/test_detail.html')
     c = Context({
         'test': test,
+    })
+    return HttpResponse(t.render(c))
+
+
+def service_plan_list(request):
+    service_plan_list = ServicePlan.objects.all().order_by('serplanno')
+    #service_plan_list = ServicePlan.objects.all()
+    t = loader.get_template('warranty/service_plan_list.html')
+    c = Context({
+        'service_plan_list': service_plan_list,
+    })
+    return HttpResponse(t.render(c))
+    #return HttpResponse("Yahoo")
+
+def service_plan_detail(request, serplanno):
+    service_plan_list = ServicePlan.objects.all()
+    service_plan = get_object_or_404(ServicePlan, pk=serplanno)
+    t = loader.get_template('warranty/service_plan_detail.html')
+    c = Context({
+        'service_plan': service_plan,
     })
     return HttpResponse(t.render(c))
